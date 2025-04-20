de7c21c1735c7ede764862d4cd88c2ab76667790
Antti Väisänen <avaisan@proton.me>
2025-04-20 19:48:04 +0300

    Separate WAF into its own stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,7 @@ import { InfrastructureStack } from '../lib/infrastructure';
 import { DatabaseStack } from '../lib/database';
 import { BackendStack } from '../lib/backend';
 import { WAFStack } from '../lib/waf';
-import { FrontendStack } from '../lib/frontend';
+//import { FrontendStack } from '../lib/frontend';
 import { getConfig } from '../lib/config';
 
 const app = new cdk.App();
@@ -33,19 +33,19 @@ const backend = new BackendStack(app, 'BackendStack', {
   webAcl: waf.apiGatewayWebAcl
 });
 
-const frontend = new FrontendStack(app, 'FrontendStack', { 
-  config,
-  webAcl: waf.cloudFrontWebAcl 
- });
+//const frontend = new FrontendStack(app, 'FrontendStack', { 
+//  config,
+//  webAcl: waf.cloudFrontWebAcl 
+// });
 
 
  //infra.addDependency(waf);
 database.addDependency(infra);
 backend.addDependency(database);
-frontend.addDependency(waf);
+//frontend.addDependency(waf);
 
 // Tagging
-const stacks = [infra, database, backend, frontend, waf];
+const stacks = [infra, database, backend, waf];
 stacks.forEach(stack => {
   cdk.Tags.of(stack).add('Project', 'ParkRun');
   cdk.Tags.of(stack).add('Environment', config.environmentName);
