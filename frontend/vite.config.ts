bb0e93f5f445900bdcbdc9b719560940d5eb3772
Antti Väisänen <avaisan@proton.me>
2025-04-24 23:03:14 +0300

    Fixing CORS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://api:3001',
+        target: process.env.NODE_ENV === 'production' 
+          ? 'https://mypspzz9o6.execute-api.eu-central-1.amazonaws.com'
+          : 'http://api:3001',
         changeOrigin: true,
         secure: false
       }
